4d217107a6c031b4d269d13227447e7f8a3c078d
leo7266@126.com <leo7266@126.com>
2020-10-07 11:14:50 +0800

    添加项目文件。

---
+++ WebSite1/上传文件/page1.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="page1.aspx.cs" Inherits="上传文件_page1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table style="width:100%;">
+                <tr>
+                    <td>
+                        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                        </asp:DropDownList>
+                    </td>
+                    <td>
+                        <asp:FileUpload ID="FileUpload1" runat="server" />
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Image ID="Image1" runat="server" ImageUrl="~/Image/image3726.jpg" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="文件上传" />
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebSite1/上传文件/page1.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class 上传文件_page1 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            DropListLoad();
+            this.DropDownList1.SelectedIndex = 0;
+            this.Image1.ImageUrl = "~/Image/image3726.jpg";
+        }
+    }
+
+    public void DropListLoad()
+    {
+        ArrayList arrList = new ArrayList();
+        arrList.Add("图片1");
+        arrList.Add("图片2");
+        arrList.Add("图片3");
+        this.DropDownList1.DataSource = arrList;
+        this.DropDownList1.DataBind();
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        int num = this.DropDownList1.SelectedIndex;
+        switch(num)
+        {
+            case 0:
+                this.Image1.ImageUrl = "~/Image/image3726.jpg";
+                break;
+            case 1:
+                this.Image1.ImageUrl = "~/Image/image3727.jpg";
+                break;
+            case 2:
+                this.Image1.ImageUrl = "~/Image/image3728.jpg";
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        bool bSave = false;
+        if(this.FileUpload1.HasFile)
+        {
+            string fileExtension = System.IO.Path.GetExtension(this.FileUpload1.FileName).ToLower();
+            string[] extensions = { ".gif", ".bmp", ".jpg", ".png" };
+            for (int i=0;i<extensions.Length;i++)
+            {
+                if(fileExtension==extensions[i])
+                {
+                    bSave = true;
+                }
+            }
+
+            if (bSave==true)
+            {
+                this.Image1.ImageUrl = "~/image/" + this.FileUpload1.FileName;
+                this.FileUpload1.SaveAs("D:\\data\\"+this.FileUpload1.FileName);
+                this.Label1.Text = "文件上传成功";
+
+            }
+        }
+    }
+}
